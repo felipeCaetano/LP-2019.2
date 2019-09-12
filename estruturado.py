78b85ad67b0c454a6411f8d267df98adf37b8d53
Felipe Caetano <felipecmelo@gmail.com>
2019-09-12 10:18:12 -0300

    Delete estruturado.py

--- estruturado.py
+++
@@ -1,7 +0,0 @@
-entrada = input("Entre com o Tipo de veículo:\n1 - carro\n2 - moto\n").lower()
-if entrada == '1' or entrada == 'carro':
-    print("Seu veículo tem até 4 rodas e 1 step")
-elif entrada == '2' or entrada == 'moto':
-    print("Seu veículo tem até 2 rodas e 0 step")
-else:
-    print("Entrada Invalida")
